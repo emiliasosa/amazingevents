be860b8355d6c37da4d81727a61f6f78a22f4123
emiliasosa <emi.sosato@gmail.com>
2022-12-28 04:12:39 -0300

    add script label to pages

--- assets/js/funciones.js
+++ assets/js/funciones.js
@@ -9,8 +9,8 @@ let callEvents = (data, data2)=>{
                     <h5 class="card-title text-center">${event.name}</h5>
                     <p class="card-text text-center">${event.description}</p>
                     <div class="d-flex justify-content-between price_btn_bottom">
-                    <p class="fs-5 txt_color_logo">Price: ${event.price}</p>
-                    <a href="./detail.html" class="btn btn-primary">See more..</a>
+                        <p class="fs-5 txt_color_logo">Price: ${event.price}</p>
+                        <a href="./detail.html" class="btn btn-primary">See more..</a>
                     </div>
                 </div>
             </div>
@@ -55,4 +55,4 @@ let bookExchangeEvents = callDetailEvents(data, 'Book Exchange')
 let cinemaEvents = callDetailEvents(data, 'Cinema')
 
 
-console.log(raceEvents)
+console.log(pastEvents)
